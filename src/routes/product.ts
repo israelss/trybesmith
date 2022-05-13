d6e01f02771c6d67d5c067ac192766d8e829d630
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-13 18:57:26 -0300

    feat: update productRoutes
    Implement `POST /products` route;
    
    Use middlewares:
     - `productValidation.name`
     - `productValidation.amount`

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { ProductController } from '../controllers';
+import { productValidation } from '../middlewares';
 
 const router = Router();
 
 const controller = new ProductController();
 
 router.get('/', controller.getAll);
+
+router.post(
+  '/',
+  productValidation.name,
+  productValidation.amount,
+  controller.create,
+);
 
 export default router;
