5cd7eebf766baf56a353ec0b0c102b187c780a16
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-13 19:59:00 -0300

    feat: export user validation middlewares from index
    That is to easily import userValidation as an object

---
+++ src/middlewares/user/index.ts
@@ -0,0 +1,11 @@
+import classe from './classe';
+import level from './level';
+import password from './password';
+import username from './username';
+
+export {
+  classe,
+  level,
+  password,
+  username,
+};
