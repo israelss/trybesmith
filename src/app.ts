07ed6f076af592d39bf017a92cea132d996d67b5
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-13 20:00:36 -0300

    feat: use userRoutes on `/users`

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productRoutes from './routes/product';
+import userRoutes from './routes/user';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 export default app;
