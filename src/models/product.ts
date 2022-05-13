0e53bc96535b808604733d005a8e715b2fd951f5
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-13 18:52:06 -0300

    feat: update ProductModel
    Implement `create` method

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,3 +1,4 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import { Product } from '../interfaces';
 
@@ -7,6 +8,13 @@ export class ProductModel {
     const [allProducts] = await connection.execute(query);
 
     return allProducts as Product[];
+  };
+
+  public create = async (product: Product): Promise<number> => {
+    const { name, amount } = product;
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+    const [result] = await connection.execute<ResultSetHeader>(query, [name, amount]);
+    return result.insertId;
   };
 }
 
