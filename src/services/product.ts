f7db0b5b6a89f70cad1e94fb5da5b93c956840a6
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-13 18:52:29 -0300

    feat: update ProductService
    Implement `create` method

--- src/services/product.ts
+++ src/services/product.ts
@@ -5,6 +5,8 @@ export class ProductService {
   model = new ProductModel();
 
   public getAll = async (): Promise<Product[]> => this.model.getAll();
+
+  public create = async (product: Product): Promise<number> => this.model.create(product);
 }
 
 export default ProductService;
