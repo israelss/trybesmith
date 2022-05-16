5198397554d60d7f94e32960d3ff6bac72643b28
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 14:53:19 -0300

    feat: use orderRoutes on `/orders`

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import orderRoutes from './routes/order';
 import productRoutes from './routes/product';
 import userRoutes from './routes/user';
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 
