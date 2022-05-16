d8090d540d03414ebf02f9b69229b484b80d48cc
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 16:36:48 -0300

    feat: update user validation middleware on index
     - Include checkUser validator

--- src/middlewares/user/index.ts
+++ src/middlewares/user/index.ts
@@ -1,9 +1,11 @@
+import checkUser from './checkUser';
 import classe from './classe';
 import level from './level';
 import password from './password';
 import username from './username';
 
 export {
+  checkUser,
   classe,
   level,
   password,
