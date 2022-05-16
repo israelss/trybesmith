d91a81b8bde52d416edb1cb533dbf885e5fe0d2d
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 20:08:36 -0300

    feat: update orderRoutes
    Implement `POST /orders` route;

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { OrderController } from '../controllers';
+import { orderValidation, userValidation } from '../middlewares';
 
 const router = Router();
 
 const controller = new OrderController();
 
 router.get('/', controller.getAll);
+
+router.post(
+  '/',
+  userValidation.token,
+  orderValidation.productsIds,
+  controller.create,
+);
 
 export default router;
