dfa37f0cf079417d3dde94892195d5bab5563625
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 20:03:32 -0300

    feat: update AuthService
    Implement `verify` method

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,5 @@
-import jwt, { Secret, SignOptions } from 'jsonwebtoken';
+import jwt, { JsonWebTokenError, JwtPayload, Secret, SignOptions } from 'jsonwebtoken';
+import { User } from '../interfaces';
 
 const SECRET: Secret = process.env.JWT_SECRET || 'jwt_default_secret_trybesmith_project';
 const JWT_CONFIG: SignOptions = { expiresIn: '7d', algorithm: 'HS256' };
@@ -8,6 +9,19 @@ export class AuthService {
     const token = jwt.sign({ data: { id, username } }, SECRET, JWT_CONFIG);
 
     return token;
+  };
+
+  static verify = (token: string | undefined): User | null => {
+    if (!token) throw new Error('Token must not be null nor undefined!');
+
+    try {
+      const { data } = jwt.verify(token, SECRET) as JwtPayload;
+      return data;
+    } catch (error) {
+      if (error instanceof JsonWebTokenError && error.message === 'jwt malformed') return null;
+      console.log(error);
+      throw error;
+    }
   };
 }
 
