dc6fd7ee9864318687d431a640ffcf40c8a1d7f9
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 16:40:02 -0300

    feat: update UserController
    Implement `login` method

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from 'express';
-import { Success } from '../enums/http_status_codes';
-import { UserService } from '../services';
+import { ClientError, Success } from '../enums/http_status_codes';
+import { UserRequest } from '../interfaces';
+import { AuthService, UserService } from '../services';
 
 export class UserController {
   service = new UserService();
@@ -9,6 +10,16 @@ export class UserController {
     const user = req.body;
     const token = await this.service.create(user);
     return res.status(Success.CREATED).json({ token });
+  };
+
+  public login = async (req: UserRequest, res: Response): Promise<Response> => {
+    const { user } = req;
+    if (user) {
+      const { id, username } = user;
+      const token = AuthService.generateToken(id, username);
+      return res.status(Success.OK).json({ token });
+    }
+    return res.status(ClientError.UNAUTHORIZED).json({ message: 'User not found!' });
   };
 }
 
