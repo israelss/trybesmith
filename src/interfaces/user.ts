92fe58243a47c8896008bf2dd8eb70f7ab4a8f9c
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 16:32:52 -0300

    feat: create UserRequest interface
    We extended Request interface to be able to:
     - inject an User object on it
     - retrieve an User object from it

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,7 +1,13 @@
+import { Request } from 'express';
+
 export interface User {
   id: number;
   username: string;
   classe: string;
   level: number;
   password: string;
+}
+
+export interface UserRequest extends Request {
+  user?: User;
 }
