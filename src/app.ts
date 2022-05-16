0d1c1b888fc2fae25973c6da5de99cc485b46457
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 16:40:51 -0300

    feat: use loginRoutes on `/login`

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import loginRoutes from './routes/login';
 import orderRoutes from './routes/order';
 import productRoutes from './routes/product';
 import userRoutes from './routes/user';
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/login', loginRoutes);
 app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
