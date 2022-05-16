8d778cb926555394b4526cc5fc46a1fc0a95ae41
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-16 20:05:35 -0300

    feat: update user validation middleware on index
     - Include `token` validator

--- src/middlewares/user/index.ts
+++ src/middlewares/user/index.ts
@@ -2,6 +2,7 @@ import checkUser from './checkUser';
 import classe from './classe';
 import level from './level';
 import password from './password';
+import token from './token';
 import username from './username';
 
 export {
@@ -9,5 +10,6 @@ export {
   classe,
   level,
   password,
+  token,
   username,
 };
